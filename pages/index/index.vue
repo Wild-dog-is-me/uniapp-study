9e20cfbebcebcb5a51e417b464f550ea4f75f13d
Wild-dog-is-me <1427774041@qq.com>
2022-12-31 21:28:30 +0800

    初始化uniapp

---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="box">
+		<h1>uniapp</h1>
+	</view>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style lang="scss">
+	
+	.box {
+		height: 100px;
+		background-color: aqua;
+		
+		h1 {
+			font-size: 40px;
+			background-color: beige;
+		}
+	}
+	
+	
+
+</style>
